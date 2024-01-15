abf25f8fc64793b9b20cc6b6f7f0900219b0d749
UshaSuwal <usaasuwa@gmail.com>
2024-01-15 14:00:40 +0545

    updated loan

--- loan.py
+++ loan.py
@@ -10,6 +10,8 @@ import streamlit as st
 
 
 
+
+
 @st.cache_data 
 def getdata():
     df=pd.read_csv("cleaned1_loan.csv",index_col=0)
@@ -28,9 +30,16 @@ y_test = df_test["loan_status"]
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression()    #model
 model.fit(X_train,y_train)
+predict_target = model.predict(X_test)    
 
 
 st.title("Loan Approval Prediction")
+
+st.header("Model used:")
+st.write("Logistic Regression")
+st.write("This loan prediction model utilizes logistic regression to evaluate loan applications, providing a outcome of either approval or rejection. It analyzes a set of input features, which could include factors such as the applicant's credit score, income, assets value, and other relevant financial information. By learning from historical data where loan outcomes are known, the logistic regression model establishes a relationship between these features and the likelihood of loan approval.")
+
+
 with st.form(key="my_form"):
    gender=st.radio("Gender", ["Male", "Female"])
    education=st.radio("Education", ["Graduate", "Ungraduate"])
@@ -67,3 +76,53 @@ if c:
         st.write("Loan Approved!")
     else:
         st.write("Loan Rejected!")
+
+
+
+st.header("Accuracy Score of Model:")
+from sklearn.metrics import accuracy_score
+
+# Assuming Y_test is the actual labels
+# and predicted_risk contains the predicted labels
+accuracy = accuracy_score(y_test,predict_target)
+st.write(f'Accuracy: {accuracy * 100:.2f}%')
+
+
+
+st.header("Classification Report:")
+from sklearn.metrics import classification_report
+
+# Assuming Y_test is the actual labels
+# and predicted_risk contains the predicted labels
+report = classification_report(y_test, predict_target)
+
+st.text("Classification Report:\n{}".format(report))
+
+
+
+st.header("Confusion Matrix:")
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+# Assuming X_test and y_test are your test data
+cm = confusion_matrix(model.predict(X_test), y_test)
+
+# Change figure size and increase dpi for better resolution
+# and get reference to axes object
+fig, ax = plt.subplots(figsize=(8, 8), dpi=100)
+class_names = ['0', '1']
+
+# initialize using the raw 2D confusion matrix
+# and output labels (in our case, it's 0 and 1)
+display = ConfusionMatrixDisplay(cm, display_labels=class_names)
+
+# set the plot title using the axes object
+
+ax.grid(False)
+
+# show the plot.
+# Pass the parameter ax to show customizations (ex. title)
+display.plot(ax=ax)
+
+# Use Streamlit to display the Matplotlib figure
+st.pyplot(fig)
