0852257114eae5b727d7401d9c6c44647c3f084c
UshaSuwal <usaasuwa@gmail.com>
2024-01-15 14:58:20 +0545

    email update

--- email.py
+++ email.py
@@ -22,6 +22,10 @@ df = df.dropna(subset=['Message'])
 
 st.title("Spam Email Detection")
 
+st.header("Model Used:")
+st.write("Logistic Regression")
+st.write("The Spam Email Classifier using Logistic Regression is a machine learning model designed to identify whether an email is legitimate (ham) or spam. Leveraging logistic regression, a binary classification algorithm, the model has been trained on a dataset of labeled emails. By analyzing various features within the email content, such as keywords. The model learns to distinguish between legitimate and spam emails.")
+st.header("Analyze:")
 from sklearn.linear_model import LogisticRegression
 log_regression = LogisticRegression()
 
@@ -37,6 +41,7 @@ p = Pipeline([('vect', vectorizer),
               ('clf', LogisticRegression())])
 
 model = p.fit(X_train, y_train)
+prediction=model.predict(X_test)
 
 
 
@@ -51,3 +56,27 @@ with st.form(key="my_form"):
 if c:
     st.write("It is ",predicted[0],"message.")
 
+
+
+
+st.header("Model Accuracy Score:")
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test,prediction)
+st.write(f'Accuracy: {accuracy * 100:.2f}%')
+
+
+st.header("Classification Report:")
+from sklearn.metrics import classification_report
+report = classification_report(y_test, prediction)
+st.text("Classification Report:\n{}".format(report))
+
+
+st.header("Confusion Matrix:")
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+cm = confusion_matrix(model.predict(X_test), y_test)
+fig, ax = plt.subplots(figsize=(8, 8), dpi=100)
+class_names = ['ham', 'spam']
+display = ConfusionMatrixDisplay(cm, display_labels=class_names)
+display.plot(ax=ax)
+st.pyplot(fig)
